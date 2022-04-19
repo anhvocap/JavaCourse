e27f3b80c331ad2c2a08d95d1a6ee8d2027137c8
anhvocap <anh.vo@capgemini.com>
2022-04-19 11:10:58 +0700

    - add project: Airplan + Hibernate

---
+++ Airplan/src/main/java/org/pluralsight/hibernate/Main.java
@@ -0,0 +1,50 @@
+package org.pluralsight.hibernate;
+
+import org.pluralsight.hibernate.airplan.Airport;
+import org.pluralsight.hibernate.airplan.Passenger;
+import org.pluralsight.hibernate.airplan.Ticket;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("HibernateAirPlan");
+        EntityManager em = emf.createEntityManager();
+
+        em.getTransaction().begin();
+        Airport airport = new Airport(1, "Henri Coanda");
+
+        Passenger john = new Passenger(1, "John Smith");
+        john.setAirport(airport);
+        Passenger mike = new Passenger(2, "Michael Johnson");
+        mike.setAirport(airport);
+        airport.addPassenger(john);
+        airport.addPassenger(mike);
+
+        Ticket ticket1 = new Ticket(1, "AA1234");
+        ticket1.setPassenger(john);
+
+        Ticket ticket2 = new Ticket(2, "BB5678");
+        ticket2.setPassenger(john);
+
+        john.addTicket(ticket1);
+        john.addTicket(ticket2);
+
+        Ticket ticket3 = new Ticket(3, "CC0987");
+        ticket3.setPassenger(mike);
+        mike.addTicket(ticket3);
+
+        em.persist(airport);
+        em.persist(john);
+        em.persist(mike);
+
+        em.persist(ticket1);
+        em.persist(ticket2);
+        em.persist(ticket3);
+
+        em.getTransaction().commit();
+        emf.close();
+    }
+}
